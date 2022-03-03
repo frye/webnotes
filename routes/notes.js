627c1c91ff605e623ff239b0b0164fb785dd7dbd
Samuli Jarvinen <frye@github.com>
2022-03-03 01:56:15 -0800

    Delete notes working

--- routes/notes.js
+++ routes/notes.js
@@ -29,10 +29,10 @@ router.post('/', (req, res) => {
 				path.join(__dirname, '../db/db.json'),
 				JSON.stringify(notesArr, null, 4),
 				(writeErr) =>
-				  writeErr
-					 ? console.error(writeErr)
-					 : console.info('Successfully updated notes!')
-			 );
+					writeErr
+						? console.error(writeErr)
+						: console.info('Successfully updated notes!')
+			);
 			const response = {
 				"status": "success",
 				"body": newNote,
@@ -42,9 +42,33 @@ router.post('/', (req, res) => {
 	});
 });
 
-	router.delete('/:id', (req, res) => {
-		let id = req.params.id.toLowerCase();
-		res.end(`Delete ${id} - Not Implemented yet!`);
+router.delete('/:id', (req, res) => {
+	let id = req.params.id.toLowerCase();
+	const json = fs.readFile(path.join(__dirname, '../db/db.json'), 'utf-8', (err, data) => {
+		if (err) {
+			console.error(err);
+		} else {
+			let notesArr = JSON.parse(data);
+			for (let i = 0; i<notesArr.length; i++) {
+				if ( notesArr[i].id === id ) {
+					notesArr.splice(i, 1);
+				}
+			}
+			fs.writeFile(
+				path.join(__dirname, '../db/db.json'),
+				JSON.stringify(notesArr, null, 4),
+				(writeErr) =>
+					writeErr
+						? console.error(writeErr)
+						: console.info('Successfully updated notes!')
+			);
+			const response = {
+				"status": "success",
+				"body": `Deleted note with id: ${id}`,
+			}
+			res.status(201).json(response);
+		}
 	});
+});
 
-	module.exports = router;
+module.exports = router;
